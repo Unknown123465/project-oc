f1605d415d9a466e44105cfa3df58037a4ac1f5d
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2026-08-21 09:52:06 +0900

    fix: lightningCssFeatures의 light-dark 기능으로 인해 light-dark 함수에 값이 올바르지 않은 변수가 삽입되서 light-dark 기능 비활성화

--- project-oc/next.config.ts
+++ project-oc/next.config.ts
@@ -1,9 +1,15 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    reactCompiler: true,
-    allowedDevOrigins: ["127.0.0.1", "localhost"],
+	/* config options here */
+	reactCompiler: true,
+	allowedDevOrigins: ["127.0.0.1", "localhost"],
+	experimental: {
+		useLightningcss: true,
+		lightningCssFeatures: {
+			exclude: ["light-dark"],
+		},
+	},
 };
 
 export default nextConfig;
