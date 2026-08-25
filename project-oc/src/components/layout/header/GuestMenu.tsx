467103f38c8c75b7fde54921499f74f0e6d43cfa
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2026-08-25 13:15:33 +0900

    [Feature] 태블릿 및 모바일 환경 헤더 토글 메뉴 구현 완 (#6)
    * fix: 데스크톱 헤더 좌우여백 불일치 수정
    
    * fix: 헤더 메뉴에 해당하는 태그를 시맨틱 태그에 맞게 수정
    
    * feat: 태블릿/모바일 일 시 헤더에 토글 메뉴 적용.

--- project-oc/src/components/layout/header/GuestMenu.tsx
+++ project-oc/src/components/layout/header/GuestMenu.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function GuestMenu() {
 	return (
-		<div className={styles.menu}>
+		<div className={styles.menu} aria-label="주요 메뉴">
 			<Link href="/" className={styles.text}>
 				프로필 둘러보기
 			</Link>
