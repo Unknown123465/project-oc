92b4de97fd184efb37fd4543f366a080a2cba942
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2026-08-27 15:27:10 +0900

    fix: html 요소에 suppressHydrationWarning 추가

--- project-oc/src/app/layout.tsx
+++ project-oc/src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({children}: LayoutProps<"/">) {
 	return (
-		<html lang="ko" className={pretendard.className}>
+		<html lang="ko" className={pretendard.className} suppressHydrationWarning>
 			<head>
 				<script
 					dangerouslySetInnerHTML={{
