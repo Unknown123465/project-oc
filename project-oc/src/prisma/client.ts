6d7d73de4a9d19b6a283c9f4ad83c6be61743226
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2026-08-30 09:44:00 +0900

    fix: DB 비밀번호 환경변수명 오타로 Linux에서 조회가 실패하는 문제 수정

--- project-oc/src/prisma/client.ts
+++ project-oc/src/prisma/client.ts
@@ -7,7 +7,7 @@ const db = new PrismaClient({
         host: env("DATABASE_HOST"),
         user: env("DATABASE_USER"),
         database: env("DATABASE_NAME"),
-        password: env("DATABASE_PASSWORd"),
+        password: env("DATABASE_PASSWORD"),
         allowPublicKeyRetrieval: true,
         logger: {
             /*network: (info) => {
