d4c5801b51f93a7187e68d8066f8e99b64a1f689
Unknown123465 <86813264+Unknown123465@users.noreply.github.com>
2026-09-08 12:01:22 +0900

    fix: 로그아웃 시 헤더 상태 깜빡임 문제 수정

--- project-oc/src/app/logout/page.tsx
+++ project-oc/src/app/logout/page.tsx
@@ -1,46 +1,41 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import logoutAction from "./action";
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import {useEffect} from "react";
+import {signOut, useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
 
 export default function Logout() {
-    const session = useSession();
+	const session = useSession();
 
-    const router = useRouter();
+	const router = useRouter();
 
-    const formRef = useRef<HTMLFormElement>(null);
+	const logout = async () => {
+		await signOut({
+			redirectTo: "/",
+		});
+	};
 
-    useEffect(() => {
-        if (session.status === "authenticated") {
-            const loop: number = window.setInterval(() => {
-                if (formRef.current !== null) {
-                    window.clearInterval(loop);
+	useEffect(() => {
+		if (session.status === "authenticated") {
+			logout();
+		} else if (session.status === "unauthenticated") {
+			router.replace("/");
+		}
+	}, [session.status]);
 
-                    formRef.current.requestSubmit();
-                }
-            }, 10);
-        } else if (session.status === "unauthenticated") {
-            router.replace("/");
-        }
-    }, [session.status]);
-
-    if (session.status === "authenticated") {
-        return (
-            <div>
-                <h1>로그아웃 중입니다...</h1>
-
-                <form action={logoutAction} hidden ref={formRef}></form>
-            </div>
-        );
-    } else if (session.status === "loading") {
-        return (
-            <div>
-                <h1>로그인 여부를 확인중입니다...</h1>
-            </div>
-        );
-    } else {
-        return <div></div>;
-    }
+	if (session.status === "authenticated") {
+		return (
+			<div>
+				<h1>로그아웃 중입니다...</h1>
+			</div>
+		);
+	} else if (session.status === "loading") {
+		return (
+			<div>
+				<h1>로그인 여부를 확인중입니다...</h1>
+			</div>
+		);
+	} else {
+		return <div></div>;
+	}
 }
